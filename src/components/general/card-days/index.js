ea352ccbf3a580c4f51aa72e43980059fe98dde0
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2024-08-21 21:33:51 -0300

    feat: implementa listagem de horarios no agendamento

--- src/components/general/card-days/index.js
+++ src/components/general/card-days/index.js
@@ -1,12 +1,30 @@
 import styles from'./cardDays.module.css'
+import React, { useState } from 'react';
 
 export default function CardDay(props){
+    const schedules = []
+    for(let time = 7; time < 19;time++){
+        schedules.push(time);
+    }
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const dayCLick = () => { setIsExpanded(!isExpanded); }
+
+    const timeClick = () => {console.log('Você selecionou um horário!')}
+
     return (
         <div className={styles.day_container}>
-            <div className={styles.day_info}>
-                <p>{ props.day.dayWord }</p>
-                <p>{ props.day.date }</p>
+            <div className={styles.day_data} onClick={dayCLick}>
+                <div className={styles.day_info}>
+                    <p>{ props.day.dayWord }</p>
+                    <p>{ props.day.date }</p>
+                </div>
+            </div>
+
+            <div className={isExpanded ? styles.day_time : styles.none}>
+                {schedules.map( (time, index) => ( <p key={index} onClick={timeClick}>{time} : 00</p> ))}
             </div>
         </div>
     );
 }
+
--- src/pages/client-side/scheduling/index.js
+++ src/pages/client-side/scheduling/index.js
@@ -11,7 +11,6 @@ function Scheduling() {
           <Header></Header>   
           <Days></Days>
 
-
       </section>
     );
   }
