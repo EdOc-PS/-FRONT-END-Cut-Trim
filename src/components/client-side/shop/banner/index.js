dce7a6fa759e24d1cc704b67fb9e47fa360b59bb
Guilherme Victor <guilhermevr.figueiredo@outlook.com>
2024-08-31 11:49:26 -0300

    feat: tornar dinamico os dados da barbearia no banner

--- src/components/client-side/shop/banner/index.js
+++ src/components/client-side/shop/banner/index.js
@@ -1,20 +1,45 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import styles from './banner.module.css'
+import styles from './banner.module.css';
+import { useState, useEffect } from 'react';
+import { Get } from '../../../../core/service/get';
 
-export default function Banner() {
+export default function Banner(props) {
+  const[barberShopName, setBarberShopName] = useState('Barbearia');
+  const[city, setCity] = useState('Ouro Branco');
+  const[street, setStreet] = useState('Rua das Flores');
+  const[number, setNumber] = useState('123');
+  const[openingTime, setOpeningTime] = useState('');
+  const[closingTime, setClosingTime] = useState('');
+
+  useEffect(() => {
+    const getBarberShops = async () => {
+      Get('http://localhost:8080/cutandtrim/barbershop/find/' + props.barberShop.id)
+        .then(jBody => {
+          if(jBody){
+            setBarberShopName(jbody.name);
+            setCity(jbody.city);
+            setStreet(jBody.street);
+            setNumber(jBody.number);
+          }
+        }).catch(error => { console.error('Error:', error); });
+    }
+
+    getBarberShops();
+  }, []);
+
   return (
 
     <section className={styles.banner_body}>
       <img src='https://t4.ftcdn.net/jpg/02/10/97/19/360_F_210971959_wXcBYfif7jKeyKkHKhVyOnzQWHawIgK4.jpg' alt='' />
 
       <div className={styles.info_container}>
-        <h1>JUBISCREIA TITULO</h1>
+        <h1>{barberShopName}</h1>
         <div className={styles.address_container}>
           <i className="fi fi-rr-marker"></i>
           <div className={styles.txt_contaienr}>
-            <p>Ouro Branco, Rua: Minas Gerais, 107</p>
-            <p className={styles.time}>07:00 AM - 16:00 PM</p>
+            <p>{city}, {street}, {number}</p>
+            <p className={styles.time}>{openingTime} AM - {closingTime} PM</p>
           </div>
         </div>
       </div>
